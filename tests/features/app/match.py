beb24f218beece0c887fa94b53f8336cec4bc490
marsanem <octet.software@gmail.com>
2014-09-18 11:07:36 -0500

    Making pretty

--- tests/features/app/match.py
+++ tests/features/app/match.py
@@ -15,10 +15,10 @@ class Match:
             return "{0} plays with {1} | 0-0".format(self.p1, self.p2)
         elif self.contp1 > self.contp2:
             return "{0} defeated {1} | {2}".format(self.p1,
-                                    self.p2, self.getpoints(self.p1))
+                self.p2, self.getpoints(self.p1))
         else:
             return "{0} defeated {1} | {2}".format(self.p2,
-                                    self.p1, self.getpoints(self.p2))
+                self.p1, self.getpoints(self.p2))
 
     def add(self, player, set_num, num1, num2):
         if player == self.p1:
@@ -36,7 +36,9 @@ class Match:
         total = self.contp1 + self.contp2
         for i in range(total):
             if player == self.p1:
-                points += str(self.scorep1[i]) + "-" + str(self.scorep2[i]) + ", "
+                points += str(self.scorep1[i]) + "-" + \
+                    str(self.scorep2[i]) + ", "
             else:
-                points += str(self.scorep2[i]) + "-" + str(self.scorep1[i]) + ", "
+                points += str(self.scorep2[i]) + "-" + \
+                    str(self.scorep1[i]) + ", "
         return points[:-2]
--- tests/features/steps.py
+++ tests/features/steps.py
@@ -7,9 +7,11 @@ import app.match as m
 def given_p1_and_p2_start_a_match_to_pacted_sets(step, p1, p2, sets):
     world.match = m.Match(p1, p2, sets)
 
+
 @step(u'Then: I see score: "([^"]*)"')
 def then_i_see_score(step, score):
     assert world.match.score() == score, "Got %s" % world.match.score()
+
 
 @step(u'When: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
 def when_player_won_the_set_num(step, p1, set_num, num1, num2):
@@ -19,6 +21,7 @@ def when_player_won_the_set_num(step, p1, set_num, num1, num2):
 @step(u'And: "([^"]*)" won the "([^"]*)" set "([^"]*)"-"([^"]*)"')
 def and_player_won_the_set_num(step, p1, set_num, num1, num2):
     world.match.add(p1, int(set_num[0]), num1, num2)
+
 
 @step(u'Then: The match score is: "([^"]*)"')
 def then_the_match_score_is_p1(step, score):
